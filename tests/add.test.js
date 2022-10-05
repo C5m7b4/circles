237f2c03ed447c0b2c6f07996c357523200045a3
C5m7b4 <mbedingfield@dcrpos.com>
2022-10-05 18:42:07 -0500

    add subtract

--- tests/add.test.js
+++ tests/add.test.js
@@ -1,5 +1,8 @@
-import { add } from '../src/index';
+import { add, subtract } from '../src/index';
 
 test('should add two numbers', () => {
   expect(add(1, 2)).toEqual(3);
+});
+test('should subtract', () => {
+  expect(subtract(2, 1)).toEqual(1);
 });
